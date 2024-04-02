fdff2fe039f0dff634cbe8bcbdebf3b1f46c2b3c
MAHTO ANJALI <159051588+MAHTO-ANJALI@users.noreply.github.com>
2024-04-02 11:25:00 +0530

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -63,9 +63,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TeamYM")
-                await self.one.join_chat("TheYukki")
-                await self.one.join_chat("YukkiSupport")
+                await self.one.join_chat("ANJALIxOFFICIAL")
+                await self.one.join_chat("ANJALIxWORLD")
+                await self.one.join_chat("AnjalixSupportxGroup")
             except:
                 pass
             assistants.append(1)
@@ -94,9 +94,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("TeamYM")
-                await self.two.join_chat("TheYukki")
-                await self.two.join_chat("YukkiSupport")
+                await self.two.join_chat("ANJALIxOFFICIAL")
+                await self.two.join_chat("ANJALIxWORLD")
+                await self.two.join_chat("AnjalixSupportxGroup")
             except:
                 pass
             assistants.append(2)
@@ -125,9 +125,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("TeamYM")
-                await self.three.join_chat("TheYukki")
-                await self.three.join_chat("YukkiSupport")
+                await self.three.join_chat("ANJALIxOFFICIAL")
+                await self.three.join_chat("ANJALIxWORLD")
+                await self.three.join_chat("AnjalixSupportxGroup")
             except:
                 pass
             assistants.append(3)
@@ -156,9 +156,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("TeamYM")
-                await self.four.join_chat("TheYukki")
-                await self.four.join_chat("YukkiSupport")
+                await self.four.join_chat("ANJALIxOFFICIAL")
+                await self.four.join_chat("ANJALIxWORLD")
+                await self.four.join_chat("AnjalixSupportxGroup")
             except:
                 pass
             assistants.append(4)
@@ -187,9 +187,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("TeamYM")
-                await self.five.join_chat("TheYukki")
-                await self.five.join_chat("YukkiSupport")
+                await self.five.join_chat("ANJALIxOFFICIAL")
+                await self.five.join_chat("ANJALIxWORLD")
+                await self.five.join_chat("AnjalixSupportxGroup")
             except:
                 pass
             assistants.append(5)
